38f849d132a45d290b387f080b1430de9e58ce59
Scott Caruso <scottcaruso@fullsail.edu>
2013-09-06 00:25:17 -0400

    Started creating XML layout for activity - have 4 of the currencies covered with placeholder images.

--- CurrencyConverter/gen/com/scottcaruso/currencyconverter/R.java
+++ CurrencyConverter/gen/com/scottcaruso/currencyconverter/R.java
@@ -24,7 +24,17 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f08000a;
+        public static final int ausdol_amt=0x7f080005;
+        public static final int ausdol_label=0x7f080004;
+        public static final int can_amt=0x7f080007;
+        public static final int can_label=0x7f080006;
+        public static final int dollar_amt=0x7f080000;
+        public static final int dollar_label=0x7f080001;
+        public static final int euro_amt=0x7f080003;
+        public static final int euro_label=0x7f080002;
+        public static final int hk_amt=0x7f080009;
+        public static final int hk_label=0x7f080008;
     }
     public static final class layout {
         public static final int activity_currency=0x7f030000;
@@ -33,9 +43,20 @@ public final class R {
         public static final int currency=0x7f070000;
     }
     public static final class string {
+        public static final int _0_00=0x7f05000a;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int aus=0x7f05000b;
+        public static final int aussie_amount=0x7f050008;
+        public static final int australian_dollars=0x7f050007;
+        public static final int can=0x7f05000c;
+        public static final int eur=0x7f050009;
+        public static final int euro_amt_=0x7f050006;
+        public static final int euros=0x7f050005;
         public static final int hello_world=0x7f050002;
+        public static final int hk=0x7f05000d;
+        public static final int placeholder_dollars=0x7f050004;
+        public static final int u_s_dollars=0x7f050003;
     }
     public static final class style {
         /** 
